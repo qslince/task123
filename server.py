a8c02c3c243e0684c0e494f18e63dba7b9855bef
qslince <120206873+qslince@users.noreply.github.com>
2023-03-31 19:29:04 +0200

    Update server.py

--- server.py
+++ server.py
@@ -1,15 +1,19 @@
-import os
-
 from flask import Flask
+from flask_login import LoginManager
+from data import db_session
+
 
 app = Flask(__name__)
+app.config['SECRET_KEY'] = 'yandexlyceum_secret_key'
+login_manager = LoginManager()
+login_manager.init_app(app)
+db_session.global_init("db/mars_explorer.db")
 
 
-@app.route("/")
-def index():
-    return "Привет от приложения Flask"
+def main():
+    db_session.global_init("db/mars_explorer.sqlite")
+    app.run()
 
 
 if __name__ == '__main__':
-    port = int(os.environ.get("PORT", 5000))
-    app.run(host='0.0.0.0', port=port)
+    main()
